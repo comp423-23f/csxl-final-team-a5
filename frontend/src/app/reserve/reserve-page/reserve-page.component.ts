d6c5f94e958604ef611b1fa2ed8362539b5a46cc
Lucas Siegel <lsiegel@unc.edu>
2023-11-05 18:13:36 -0500

    Expand Documentation

--- frontend/src/app/reserve/reserve-page/reserve-page.component.ts
+++ frontend/src/app/reserve/reserve-page/reserve-page.component.ts
@@ -2,7 +2,8 @@
  * The Reserve Page Component serves as a location for students to reserve seats in the
  * CSXL. Students are able to reserve seats up to one week in advance, are capped on the
  * number of hours they are able to reserve per week, and only 50% of the seats are able
- * to be reserved.
+ * to be reserved. Desks that are not pre-reserved for the next hour will be available
+ * for drop-in use as well.
  *
  * @author Bernie Chen, Kene Ochuba, Lucas Siegel, Sunny Wang
  * @copyright 2023
