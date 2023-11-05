dd820dccdef37fab9b6f9ff63a1e187fae59ab0f
Lucas Siegel <lsiegel@unc.edu>
2023-11-05 09:56:33 -0500

    Add Page Documentation

--- frontend/src/app/reserve/reserve-page/reserve-page.component.ts
+++ frontend/src/app/reserve/reserve-page/reserve-page.component.ts
@@ -1,3 +1,14 @@
+/**
+ * The Reserve Page Component serves as a location for students to reserve seats in the
+ * CSXL. Students are able to reserve seats up to one week in advance, are capped on the
+ * number of hours they are able to reserve per week, and only 50% of the seats are able
+ * to be reserved.
+ *
+ * @author Bernie Chen, Kene Ochuba, Lucas Siegel, Sunny Wang
+ * @copyright 2023
+ * @license MIT
+ */
+
 import { Component, OnInit } from '@angular/core';
 import { Route, ActivatedRoute, Router } from '@angular/router';
 
@@ -7,6 +18,7 @@ import { Route, ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./reserve-page.component.css']
 })
 export class ReservePageComponent {
+  /** Route information to be used in Organization Routing Module */
   public static Route = {
     path: 'reserve',
     title: 'Reserve',
